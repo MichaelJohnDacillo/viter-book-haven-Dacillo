ca9758bd9ba2f918e00a234ababce883621b60d1
unknown <aldrin.brinas@frontlinebusiness.com.ph>
2025-07-16 16:01:58 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./components/pages/home/Home";
+import Header from "./components/partials/Header";
+
+function App() {
+
+  return (
+    <>
+        <div className="min-h-screen bg-gray-50">
+          <Header/>
+          <Home/>
+        </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Faq from './faq/Faq'
+
+const Home = () => {
+  return (
+    <Faq/>
+  )
+}
+
+export default Home
